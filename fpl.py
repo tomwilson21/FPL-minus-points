0cce64e766f50eba57934e1d376fa95eb543b016
tomwilson21 <tomw2107@gmail.com>
2022-08-22 21:41:31 +0100

    Initial commit

---
+++ fpl.py
@@ -0,0 +1,135 @@
+from operator import ge
+from re import T
+import requests
+import json
+import pprint
+
+url = "https://fantasy.premierleague.com/api/"
+
+def get_league():
+    ext = "leagues-classic/152825/standings/"
+    #ext = "leagues-classic/893700/standings/"
+
+    response = requests.get(url+ext)
+    if response.status_code != 200:
+        raise Exception("Reponse code was "+str(response.status_code))
+    data = json.loads(response.text)
+    return data
+
+def get_teams(league_data):
+    teams = []
+    for i in league_data.keys():
+        if i == "standings":
+            for j in league_data[i].keys():
+                if j == "results":
+                    for team in league_data[i][j]:
+                        teams.append(team)
+    return teams
+
+def get_entry_id(team):
+    for k in team.keys():
+        if k == "entry":
+            return team[k]
+
+def get_team_name(team):
+     for k in team.keys():
+        if k == "entry_name":
+            return team[k]   
+
+def get_picks(entry_id, gameweek):
+    ext = "entry/{}/event/{}/picks/".format(entry_id, gameweek)
+    response = requests.get(url+ext)
+    if response.status_code != 200:
+        raise Exception("Reponse code was "+str(response.status_code))
+    data = json.loads(response.text)
+    all_picks = []
+    for i in data.keys():
+        if i == "picks":
+            for j in data[i]:
+                all_picks.append(j)
+    return all_picks
+
+def get_playing_picks(all_picks):
+    picks = []
+    for i in all_picks:
+        for j in i:
+            if j == "multiplier" and i[j] > 0:
+                picks.append(i)
+    return picks
+
+def get_player_ids(picks):
+    player_ids = []
+    for i in picks:
+        for j in i.keys():
+            if j == "element":
+                player_ids.append(i[j])
+    return player_ids
+
+def get_player_gameweek_stats(player_id, gameweek):
+    ext = "element-summary/{}/".format(player_id)
+    response = requests.get(url+ext)
+    if response.status_code != 200:
+        raise Exception("Reponse code was "+str(response.status_code))
+    data = json.loads(response.text)
+    for i in data.keys():
+        if i == "history":
+            for fixture in data[i]:
+                for k in fixture.keys():
+                    if k == "round" and fixture[k] == int(gameweek):
+                        return fixture
+
+def calculate_penalties(fixture):
+    yellow_cards = 0
+    red_cards = 0
+    own_goals = 0
+    for i in fixture:
+        if i == "yellow_cards":
+            yellow_cards += fixture[i]
+        if i == "red_cards":
+            red_cards += fixture[i]
+        if i == "own_goals":
+            own_goals += fixture[i]
+    penalty = yellow_cards + (red_cards*3) + (own_goals*2)
+    return penalty
+
+def main():
+    overall_scores = {}
+    gameweeks = ["1", "2", "3"]
+    league_data = get_league()
+    teams = get_teams(league_data)
+    for gameweek in gameweeks:
+        gameweek_scores = {}
+        print("Gameweek: {}".format(gameweek))
+        for team in teams:
+            team_name = get_team_name(team)
+            entry_id = get_entry_id(team)
+            all_picks = get_picks(entry_id, gameweek)
+            picks = get_playing_picks(all_picks)
+            player_ids = get_player_ids(picks)
+            penalty = 0
+            for player in player_ids:
+                fixture = get_player_gameweek_stats(player, gameweek)
+                penalty += calculate_penalties(fixture)
+            if team_name not in gameweek_scores.keys():
+                gameweek_scores[team_name] = penalty
+            else:
+                gameweek_scores[team_name] += penalty
+            
+            if team_name not in overall_scores.keys():
+                overall_scores[team_name] = penalty
+            else:
+                overall_scores[team_name] += penalty
+        for i in sorted(gameweek_scores, key=gameweek_scores.get, reverse=True):
+            print(i, gameweek_scores[i])
+
+    print("\n Overall Scores:")
+    for i in sorted(overall_scores, key=overall_scores.get, reverse=True):
+            print(i, overall_scores[i])
+            
+
+    
+main()
+
+#automatically populate live gameweeks
+# sort results like a table
+#find out how automatic subs work, does the multiplier automatically update? yes it does
